bd1d969c1109e96f50d8fb917c5ff2e25383bc73
AuditClay <42394776+AuditClay@users.noreply.github.com>
2022-12-14 08:43:54 -0600

    Create authorIncludes.ps1

---
+++ pester/authorIncludes.ps1
@@ -0,0 +1,14 @@
+# Short Prompt:
+function prompt{"PS > "}
+
+# Get-history:
+function gh {(Get-History).CommandLine}
+
+# Copy last command in fenced code block:
+function lb {"```````n" + ((Get-History).CommandLine | Select-Object -Last 1) + "`n```````n" | clip.exe}
+
+# Copy last command:
+function l {(Get-History).CommandLine | Select-Object -Last 1 | clip.exe}
+
+# Set-location as code block 
+function loc { ("```````nSet-Location " + (Get-Location).Path) + "`n``````"| clip.exe }

78e9766989573079ca3718ef1489f68cd58427d6
AuditClay <42394776+AuditClay@users.noreply.github.com>
2023-03-10 13:34:03 -0600

    Update Ubuntu.acceptance.tests.ps1

--- pester/Ubuntu.acceptance.tests.ps1
+++ pester/Ubuntu.acceptance.tests.ps1
@@ -12,7 +12,7 @@ Describe 'Acceptance Testing for Win10 VM' {
     Context 'Binary Existence Testing' {
       It 'ARPing exists' {
         $res = (/usr/sbin/arping )
-        $res | Should -MatchExactly "^ARPing"
+        $res | Should -MatchExactly "^ARPing.*"
       }
     }
   }

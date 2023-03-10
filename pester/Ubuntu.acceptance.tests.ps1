74b420f74d5cfb70fb1b9f693896bc11abea5317
AuditClay <42394776+AuditClay@users.noreply.github.com>
2023-03-10 13:32:13 -0600

    Update Ubuntu.acceptance.tests.ps1

--- pester/Ubuntu.acceptance.tests.ps1
+++ pester/Ubuntu.acceptance.tests.ps1
@@ -1,3 +1,12 @@
+# Invoke this test on Ubuntu with these commands (in pwsh):
+<#
+Set-Location /home/student/AUD507-Labs/pester/
+$config=New-PesterConfiguration
+$config.Output.Verbosity='detailed'
+$config.Run.Path='./Ubuntu.Setup.tests.ps1'
+Invoke-Pester -Configuration $config
+#>
+
 Describe 'Acceptance Testing for Win10 VM' {
   
     Context 'Binary Existence Testing' {

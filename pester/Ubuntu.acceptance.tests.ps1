f8a02b213e68d99d54947074a2af1b4c8269203b
AuditClay <42394776+AuditClay@users.noreply.github.com>
2023-03-10 13:33:04 -0600

    Update Ubuntu.acceptance.tests.ps1

--- pester/Ubuntu.acceptance.tests.ps1
+++ pester/Ubuntu.acceptance.tests.ps1
@@ -3,7 +3,7 @@
 Set-Location /home/student/AUD507-Labs/pester/
 $config=New-PesterConfiguration
 $config.Output.Verbosity='detailed'
-$config.Run.Path='./Ubuntu.Setup.tests.ps1'
+$config.Run.Path='./Ubuntu.acceptance.tests.ps1'
 Invoke-Pester -Configuration $config
 #>
 

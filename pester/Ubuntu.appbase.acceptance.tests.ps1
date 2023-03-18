2bd513398ebf095cc227b6dcc348138c866b0fb1
Zellok <Zellok@users.noreply.github.com>
2023-03-18 00:12:05 -0500

    pestering

--- pester/Ubuntu.appbase.acceptance.tests.ps1
+++ pester/Ubuntu.appbase.acceptance.tests.ps1
@@ -14,6 +14,11 @@ Describe 'Acceptance Testing for Win10 VM' {
         dpkg -l | grep -ci arping | Should -Be 1
       }
 
+      ## TODO: Regex be scary.. looking for docker-ce but not docker-ce-cli or docker-ce-rootless-extras
+      It 'Docker' {
+        dpkg -l | grep -cE "docker-ce([^-])" | Should -Be 1
+      }
+
       It 'Microk8s snap' {
         $res = (snap list | grep -c "^microk8s.*classic$")
         $res | Should -Be 1
@@ -28,13 +33,32 @@ Describe 'Acceptance Testing for Win10 VM' {
         gem list | grep -i cfn-nag | Should -Be 1
       }
 
+      
+
     }
 
     Context 'Downloaded software' {
-            It 'AWS CLI v2' {
+      It 'AWS CLI v2' {
         $res = (/usr/local/bin/aws --version)
         $res | Should -Match "^aws-cli/2.*"
       }
+
+      It 'Azure-Cli'{
+        $res = (/usr/bin/az --version | grep -c "^azure-cli.*")
+        $res | Should -Be 1
+      }
+
+      It 'fleet' {
+        $res = (/usr/bin/fleet version | grep -c "^fleet version.*")
+        $res | Should -Be 1
+      }
+      
+      It 'fleetctl' {
+        $res = (/usr/bin/fleetctl --version | grep -ci "^fleetctl.*version.*")
+        $res | Should -Be 1
+      }
+
+
     }
 
     Context 'File Existence' {
@@ -42,6 +66,10 @@ Describe 'Acceptance Testing for Win10 VM' {
         "/home/student/build.txt" | Should -FileContentMatch "^[0-9]{4}-.*"
         "/home/student/build.txt" | Should -FileContentMatch "^Jenkins.*"
         "/home/student/build.txt" | Should -FileContentMatch "^Github.*"
+      }
+
+      It 'Etc Issue'{
+        "/etc/issue" | Should -FileContentMatch "'^This VM hosts.*5x7.*'"
       }
       
     }
--- pester/Win10.appbase.acceptance.tests.ps1
+++ pester/Win10.appbase.acceptance.tests.ps1
@@ -2,251 +2,179 @@
 
  
 
-Describe 'Acceptance Testing for Win10 VM - AppBase' {
+Describe 'Acceptance Testing for Win10 VM - AppBase' { 
 
- 
-
-    BeforeAll {
-  
-   
-  
-      if( -Not (Test-Path -Path "C:\Program Files\osquery\osqueryi.exe" -PathType Leaf))
-  
-      {
-  
-        exit
-  
+    BeforeAll {   
+      if( -Not (Test-Path -Path "C:\Program Files\osquery\osqueryi.exe" -PathType Leaf)){  
+        exit  
       }
-  
-   
-  
-      $softwarePrograms = osqueryi 'select name,version from programs;' --json | ConvertFrom-Json
-  
+
+      $softwarePrograms = osqueryi 'select name,version from programs;' --json | ConvertFrom-Json  
       $softwareChoco = osqueryi 'select name,version from chocolatey_packages;' --json | ConvertFrom-Json
-  
-   
   
     }
    
   
     Context 'Software Existence Testing' {
-  
-   
-  
       It 'Assessor-GUI.exe Exists' {
-  
-        Test-Path -Path "C:\tools\CIS-CAT\Assessor-GUI.exe" -PathType Leaf | Should -BeTrue
-  
+          Test-Path -Path "C:\tools\CIS-CAT\Assessor-GUI.exe" -PathType Leaf | Should -BeTrue  
       }
   
    
   
-      It 'AWS CLI Exists' {
-  
-        $softwareChoco.Name | Should -Contain "awscli"
-  
+      It 'AWS CLI Exists' {  
+        $softwareChoco.Name | Should -Contain "awscli"  
       }
   
    
   
-      It 'Azure CLI Exists' {
-  
-        $softwareChoco.Name | Should -Contain "azure-cli"
-  
+      It 'Azure CLI Exists' {  
+        $softwareChoco.Name | Should -Contain "azure-cli"  
       }
   
    
   
-      It 'Burp Suite Professional Exists' {
-  
-        $softwarePrograms.Name | Should -Contain "Burp Suite Professional 2022.9.5"
-  
+      It 'Burp Suite Professional Exists' {  
+        $softwarePrograms.Name | Should -Contain "Burp Suite Professional 2022.9.5"  
       }
   
    
   
-      It 'Firefox Exists' {
-  
-        $softwareChoco.Name | Should -Contain "FirefoxESR"
-  
+      It 'Firefox Exists' {  
+        $softwareChoco.Name | Should -Contain "FirefoxESR"  
       }
   
    
   
-      It 'Google Cloud SDK Exists' {
-  
-        $softwareChoco.Name | Should -Contain "gcloudsdk"
-  
+      It 'Google Cloud SDK Exists' {  
+        $softwareChoco.Name | Should -Contain "gcloudsdk"  
       }
   
    
   
-      It 'Git CLI Exists' {
-  
-        $softwareChoco.Name | Should -Contain "git.install"
-  
+      It 'Git CLI Exists' {  
+        $softwareChoco.Name | Should -Contain "git.install"  
       }
   
    
   
-      It 'jq Exists' {
-  
-        $softwareChoco.Name | Should -Contain "jq"
-  
+      It 'jq Exists' {  
+        $softwareChoco.Name | Should -Contain "jq"  
       }
   
    
   
-      It 'OpenOffice Exists' {
-  
-        $softwareChoco.Name | Should -Contain "OpenOffice"
-  
+      It 'OpenOffice Exists' {  
+        $softwareChoco.Name | Should -Contain "OpenOffice"  
       }
   
    
   
-      It 'OpenVPN Exists' {
-  
-        $softwareChoco.Name | Should -Contain "openvpn-connect"
-  
+      It 'OpenVPN Exists' {  
+        $softwareChoco.Name | Should -Contain "openvpn-connect"  
       }
   
    
   
       It 'OpenOffice Exists' {
-  
-        $softwareChoco.Name | Should -Contain "OpenOffice"
-  
+        $softwareChoco.Name | Should -Contain "OpenOffice"  
       }
   
    
   
-      It 'Osqueryd Exists' {
-  
-        (Get-Service -Name osqueryd | Select-Object StartType).StartType | Should -BeExactly "Automatic"
-  
-        (Get-Service -Name osqueryd | Select-Object Status).Status | Should -BeExactly "Running"
-  
+      It 'Osqueryd Exists' {  
+        (Get-Service -Name osqueryd | Select-Object StartType).StartType | Should -BeExactly "Automatic"  
+        (Get-Service -Name osqueryd | Select-Object Status).Status | Should -BeExactly "Running"  
       }
   
    
   
-      It 'PowerShell Core Exists' {
-  
-        $softwareChoco.Name | Should -Contain "powershell-core"
-  
+      It 'PowerShell Core Exists' {  
+        $softwareChoco.Name | Should -Contain "powershell-core"  
       }
   
    
   
       It 'Pup Exists' {
-  
-        $softwareChoco.Name | Should -Contain "pup"
+          $softwareChoco.Name | Should -Contain "pup"
   
       }
   
    
   
-      It 'Pup Exists' {
-  
+      It 'Pup Exists' {  
         $softwareChoco.Name | Should -Contain "python3"
   
       }
   
    
   
-      It 'RVTools Exists' {
-  
-        $softwareChoco.Name | Should -Contain "rvtools"
-  
+      It 'RVTools Exists' {  
+        $softwareChoco.Name | Should -Contain "rvtools"  
       }
   
    
   
-      It 'SetDefaultBrowser Exists' {
-  
-        $softwareChoco.Name | Should -Contain "setdefaultbrowser"
-  
+      It 'SetDefaultBrowser Exists' {  
+        $softwareChoco.Name | Should -Contain "setdefaultbrowser"  
       }
   
    
   
-      It 'SoapUI Exists' {
-  
-        $softwareChoco.Name | Should -Contain "soapui"
-  
+      It 'SoapUI Exists' {  
+        $softwareChoco.Name | Should -Contain "soapui"  
       }
   
    
   
-      It 'Sslyze.exe Exists' {
-  
-        Test-Path -Path "C:\Tools\sslyze\sslyze.exe" -PathType Leaf | Should -BeTrue
-  
+      It 'Sslyze.exe Exists' {  
+        Test-Path -Path "C:\Tools\sslyze\sslyze.exe" -PathType Leaf | Should -BeTrue  
       }
   
    
   
-      It 'Windows Terminal Exists' {
-  
-        $softwareChoco.Name | Should -Contain "microsoft-windows-terminal"
-  
+      It 'Windows Terminal Exists' {  
+        $softwareChoco.Name | Should -Contain "microsoft-windows-terminal"  
       }
   
    
   
-      It 'Terraform Exists' {
-  
-        $softwareChoco.Name | Should -Contain "terraform"
-  
+      It 'Terraform Exists' {  
+        $softwareChoco.Name | Should -Contain "terraform"  
       }
   
    
   
-      It 'VSCode Exists' {
-  
-        $softwareChoco.Name | Should -Contain "vscode"
-  
+      It 'VSCode Exists' {  
+        $softwareChoco.Name | Should -Contain "vscode"  
       }
   
    
   
-      It 'Wget Exists' {
-  
-        $softwareChoco.Name | Should -Contain "Wget"
-  
+      It 'Wget Exists' {  
+        $softwareChoco.Name | Should -Contain "Wget"  
       }
   
    
   
-      It 'Zoomit Exists' {
-  
-        $softwareChoco.Name | Should -Contain "zoomit"
-  
+      It 'Zoomit Exists' {  
+        $softwareChoco.Name | Should -Contain "zoomit"  
       }
   
-    }
-  
+    } 
    
   
-    Context 'Firefox Addin Testing' { 
+    Context 'Firefox Addin Testing' {
   
-        
-  
-      It 'Wappalyzer Exists' {
-  
-        "C:\Program Files\Mozilla Firefox\distribution\policies.json" | Should -FileContentMatch 'wappalyzer'
-  
+      It 'Wappalyzer Exists' {  
+        "C:\Program Files\Mozilla Firefox\distribution\policies.json" | Should -FileContentMatch 'wappalyzer'  
       }
   
    
   
-      It 'FoxyProxy Exists' {
-  
-        "C:\Program Files\Mozilla Firefox\distribution\policies.json" | Should -FileContentMatch 'foxyproxy'
-  
+      It 'FoxyProxy Exists' {  
+        "C:\Program Files\Mozilla Firefox\distribution\policies.json" | Should -FileContentMatch 'foxyproxy'  
       }
-  
    
   
       ## TODO: Retire js

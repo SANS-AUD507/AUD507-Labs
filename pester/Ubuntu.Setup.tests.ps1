08c125de052b2c753186690443d0402da3946b69
AuditClay <42394776+AuditClay@users.noreply.github.com>
2023-04-30 08:07:23 -0500

    Update Ubuntu.Setup.tests.ps1

--- pester/Ubuntu.Setup.tests.ps1
+++ pester/Ubuntu.Setup.tests.ps1
@@ -1,6 +1,7 @@
 
 #Invoke this test on Ubuntu with these commands (in pwsh):
-<#Set-Location /home/student/AUD507-Labs/pester/
+<#
+Set-Location /home/student/AUD507-Labs/pester/
 $config=New-PesterConfiguration
 $config.Output.Verbosity='detailed'
 $config.Run.Path='./Ubuntu.Setup.tests.ps1'

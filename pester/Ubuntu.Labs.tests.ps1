60da1b9d109d306d11bff90c20d45e924a716415
AuditClay <42394776+AuditClay@users.noreply.github.com>
2023-05-04 16:17:58 -0500

    Update Ubuntu.Labs.tests.ps1

--- pester/Ubuntu.Labs.tests.ps1
+++ pester/Ubuntu.Labs.tests.ps1
@@ -20,32 +20,32 @@ Describe '507 Labs'{
 
   Context 'Lab 1.2' {
     It 'Part 1 - Host count with ARP' {
-      $hostCount = ("sudo nmap -sn -n 10.50.7.20-110 | grep -c '^Host is up'")
+      $hostCount = (sudo nmap -sn -n 10.50.7.20-110 | grep -c '^Host is up' )
       $hostCount | Should -be 12
     }
 
     It 'Part 1 - Host count without ARP' {
-      $hostCount = ("sudo nmap -sn -n --disable-arp-ping 10.50.7.20-110 | grep -c '^Host is up'")
+      $hostCount = (sudo nmap -sn -n --disable-arp-ping 10.50.7.20-110 | grep -c '^Host is up' )
       $hostCount | Should -be 11
     }
 
     It 'Part 2 - Stealth scan gets filtered port 80' {
-      $portCount = ( "sudo nmap -sS -p 80 10.50.7.26 | grep -c 'filtered'")
+      $portCount = ( sudo nmap -sS -p 80 10.50.7.26 | grep -c 'filtered' 
       $portCount | Should -Be 1
     }
 
     It 'Part 2 - Connect scan gets open port 80' {
-      $portCount = ( "sudo nmap -sT -p 80 10.50.7.26 | grep -c 'open'")
+      $portCount = ( sudo nmap -sT -p 80 10.50.7.26 | grep -c 'open' )
       $portCount | Should -Be 1
     }
     
     It 'Part 3 - OpenSSH version is 8.9p1' {
-      $portCount = ( "sudo nmap -sV -sT -p 22 10.50.7.20-25 | grep -c '8.9p1'" )
+      $portCount = ( sudo nmap -sV -sT -p 22 10.50.7.20-25 | grep -c '8.9p1' )
       $portCount | Should -Be 6
     }
   
     It 'Part 3 - Kubectl shows 4 services' {
-      $portList = ( "microk8s kubectl get services | awk -F: '/NodePort/ {print `$2}' | sed -e 's/\/.*//'" )
+      $portList = ( microk8s kubectl get services | awk -F: '/NodePort/ {print `$2}' | sed -e 's/\/.*//' )
       $portList | Should -Contain 30020
       $portList | Should -Contain 30022
       $portList | Should -Contain 30023
@@ -53,7 +53,7 @@ Describe '507 Labs'{
     }
 
     It 'Part 3 - K8s Apache versions correct' {
-      $verList = ( "sudo nmap -sT -p30022-30024 -sV 127.0.0.1 | awk '/Apache/ {print `$6}'" )
+      $verList = ( sudo nmap -sT -p30022-30024 -sV 127.0.0.1 | awk '/Apache/ {print `$6}' )
       $verList[0] | Should -Be '2.4.7'
       $verList[1] | Should -Be '2.4.7'
       $verList[2] | Should -Be '2.4.25'
@@ -62,7 +62,7 @@ Describe '507 Labs'{
 
   Context 'Lab 1.3' {
     It 'Part 3 - First user is Amartinez' {
-      $username = ( "aws iam list-users --query 'Users[*].{username:UserName}' | jq '.[0].username'" )
+      $username = ( aws iam list-users --query 'Users[*].{username:UserName}' | jq '.[0].username' )
       $username | Should -BeLike '*AMartinez*' 
           
     }

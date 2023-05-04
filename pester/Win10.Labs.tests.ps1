53f574ab11efe3a657fd2e4639badcc9a0f40123
AuditClay <42394776+AuditClay@users.noreply.github.com>
2023-05-04 16:26:26 -0500

    Update Win10.Labs.tests.ps1

--- pester/Win10.Labs.tests.ps1
+++ pester/Win10.Labs.tests.ps1
@@ -36,48 +36,6 @@ Describe '507 Labs'{
     }
   }
 
-  Context 'Lab 1.2' {
-    It 'Part 1 - Host count with ARP' {
-      $hostCount = (ssh -i C:\users\student\.ssh\ubuntukey student@ubuntu "sudo nmap -sn -n 10.50.7.20-110 | grep -c '^Host is up'")
-      $hostCount | Should -be 12
-    }
-
-    It 'Part 1 - Host count without ARP' {
-      $hostCount = (ssh -i C:\users\student\.ssh\ubuntukey student@ubuntu "sudo nmap -sn -n --disable-arp-ping 10.50.7.20-110 | grep -c '^Host is up'")
-      $hostCount | Should -be 11
-    }
-
-    It 'Part 2 - Stealth scan gets filtered port 80' {
-      $portCount = ( ssh -i C:\users\student\.ssh\ubuntukey student@ubuntu "sudo nmap -sS -p 80 10.50.7.26 | grep -c 'filtered'")
-      $portCount | Should -Be 1
-    }
-
-    It 'Part 2 - Connect scan gets open port 80' {
-      $portCount = ( ssh -i C:\users\student\.ssh\ubuntukey student@ubuntu "sudo nmap -sT -p 80 10.50.7.26 | grep -c 'open'")
-      $portCount | Should -Be 1
-    }
-    
-    It 'Part 3 - OpenSSH version is 8.9p1' {
-      $portCount = ( ssh -i C:\users\student\.ssh\ubuntukey student@ubuntu "sudo nmap -sV -sT -p 22 10.50.7.20-25 | grep -c '8.9p1'" )
-      $portCount | Should -Be 6
-    }
-  
-    It 'Part 3 - Kubectl shows 4 services' {
-      $portList = ( ssh -i C:\users\student\.ssh\ubuntukey student@ubuntu "microk8s kubectl get services | awk -F: '/NodePort/ {print `$2}' | sed -e 's/\/.*//'" )
-      $portList | Should -Contain 30020
-      $portList | Should -Contain 30022
-      $portList | Should -Contain 30023
-      $portList | Should -Contain 30024
-    }
-
-    It 'Part 3 - K8s Apache versions correct' {
-      $verList = ( ssh -i C:\users\student\.ssh\ubuntukey student@ubuntu "sudo nmap -sT -p30022-30024 -sV 127.0.0.1 | awk '/Apache/ {print `$6}'" )
-      $verList[0] | Should -Be '2.4.7'
-      $verList[1] | Should -Be '2.4.7'
-      $verList[2] | Should -Be '2.4.25'
-    }
-  }
-
   Context 'Lab 1.3' {
     It 'Part 3 - First user is Amartinez' {
       $username = ( ssh -i C:\users\student\.ssh\ubuntukey student@ubuntu "aws iam list-users --query 'Users[*].{username:UserName}' | jq '.[0].username'" )

634efca436521cd24592347077231acc1d190255
AuditClay <42394776+AuditClay@users.noreply.github.com>
2023-05-17 11:02:00 -0500

    Update Win10.Setup.tests.ps1

--- pester/Win10.Setup.tests.ps1
+++ pester/Win10.Setup.tests.ps1
@@ -19,18 +19,18 @@ Describe 'Lab Setup tests for 507Win10 VM' {
   Context 'Network connectivity' {
     It 'Ping 507Ubuntu - HostOnly' {
         $res = Test-NetConnection -ComputerName ubuntu
-        $res | Should -BeTrue
+        $res | Should -BeTrue -Because 'Ensure that second network adapter is set to Host-only'
     }
 
     It 'Ping Google - NAT' {
         $res = Test-NetConnection -ComputerName dns.google
-        $res | Should -BeTrue
+        $res | Should -BeTrue -Because 'Ensure that first network adapter is set to NAT'
     }
   }
 
   Context 'Local system checks' {
     It 'Drive free space > 10GB' {
-        (Get-PSDrive -name c).Free | Should -BeGreaterThan 10000000000
+        (Get-PSDrive -name c).Free | Should -BeGreaterThan 10000000000 -Because 'VM disk is low on space'
     }
   }
 
@@ -42,7 +42,7 @@ Describe 'Lab Setup tests for 507Win10 VM' {
     }
 
     It 'Retire.js' {
-        $plugins.identifier | Should -Contain '@retire.js'
+        $plugins.identifier | Should -Contain '@retire.js' 
     }
 
     It 'Wappalyzer' {
